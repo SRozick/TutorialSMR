46656a5209095fc8aba22a3b9817fc815f7424f7
SRozick <steven.rozick@teachafish.org>
2014-07-21 13:47:23 -0700

    Test and add static pages Home Help About
    Test presence of and specific content in Home Help and About pages.  Modify each page default to meet content requirements.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,26 @@
-=begin
-require 'rails_helper'
+# spec_helper configures all the helper files for rspec
+# this file configures all the tests for this project
+require 'spec_helper'
 
-RSpec.describe "StaticPages", :type => :request do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      get static_pages_index_path
-      expect(response.status).to be(200)
-    end
+describe "Static pages" do
+
+# test static pages/ about page for content About Us
+describe "About page" do
+  it "should have the content 'About Us'" do
+    visit '/static_pages/about'
+    expect(page).to have_content('About Us')
   end
 end
-=end
 
-require 'spec_helper'
-describe "Static pages" do
+# test static pages/ help page for content Help
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+# test static pages/ home page for content Sample App
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
