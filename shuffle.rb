b38627af29639c0e39017ffd73751d6a6373d74a
SRozick <steven.rozick@teachafish.org>
2014-08-05 10:15:53 -0400

    Shuffle Exercise

---
+++ shuffle.rb
@@ -0,0 +1,7 @@
+class String
+  def shuffle1
+    self.split('').join.shuffle
+  end
+end
+
+
---
+++ shuffle1.rb
@@ -0,0 +1,10 @@
+class String
+  def shuffle1
+    a = self.split('')
+    a = self.join
+    a = self.shuffle
+    return a
+  end
+end
+
+
